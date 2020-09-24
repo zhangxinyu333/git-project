525db73e1da2e2b4bd4bbbee17a733d0c73fab28
Fred <70703650+zhangxinyu333@users.noreply.github.com>
2020-09-24 20:40:54 +0800

    修改HelloWorld 22222

--- src/main/java/com/wk/git/Test.java
+++ src/main/java/com/wk/git/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("11111111");
+        System.out.println("2222222222");
     }
 }
